4b2998b2c7b71f8050813193782059d9f327ada3
NILESH KHABADE <83622118+NILESHKHABADE08@users.noreply.github.com>
2023-09-13 09:26:18 +0530

    Update StreamlitAppImerys.py

--- StreamlitAppImerys.py
+++ StreamlitAppImerys.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.header("Welcome to the Imerys Streamlit App Test")
+with st.form(key='my_form'):
+  username = st.text_input('Username')
+  password = st.text_input('Password')
+st.form_submit_button('Login')
 st.button('Click me')
